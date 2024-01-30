eb64828a1a99564b984f9aee1730947c18b09f30
Volodymyr <vladimir.selevertov@gmail.com>
2024-01-30 19:57:02 +0200

    init

---
+++ database/factories/ChirpFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Chirp>
+ */
+class ChirpFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->title(),
+            'user_id' => User::all()->random()->id,
+            'excerpt' => fake()->text(128),
+            'description' => fake()->text(512)
+        ];
+    }
+}
