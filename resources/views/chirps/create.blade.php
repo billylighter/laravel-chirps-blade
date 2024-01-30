67df22c62b50d61735d7fcc3410fc66d87e419d3
Volodymyr <vladimir.selevertov@gmail.com>
2024-01-30 20:29:50 +0200

    remove spaces for textarea tags

--- resources/views/chirps/create.blade.php
+++ resources/views/chirps/create.blade.php
@@ -14,17 +14,13 @@
                 <textarea
                     name="excerpt"
                     placeholder="{{ __('Excerpt goes here?') }}"
-                    @class(['border-red-500' => $errors->has('excerpt'), 'block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm'])>
-                    {{ old('excerpt') }}
-                </textarea>
+                    @class(['border-red-500' => $errors->has('excerpt'), 'block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm'])>{{ old('excerpt') }}</textarea>
                 <x-input-error :messages="$errors->get('excerpt')" class="mt-2"/>
             </div>
             <div class="mb-1">
                 <textarea
                     name="description"
-                    placeholder="{{ __('Description goes here') }}"@class(['border-red-500' => $errors->has('description'), 'block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm'])>
-                    {{ old('description') }}
-                </textarea>
+                    placeholder="{{ __('Description goes here') }}" @class(['border-red-500' => $errors->has('description'), 'block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm'])>{{ old('description') }}</textarea>
                 <x-input-error :messages="$errors->get('description')" class="mt-2"/>
             </div>
             <x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
